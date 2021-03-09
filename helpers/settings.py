c3c1934817f8dbbf49426342d2a6648e85efe894
Nate <admin@sandboxedthoughts.com>
2021-03-09 14:00:18 -0500

    add py settings for watcher

---
+++ helpers/settings.py
@@ -0,0 +1,5 @@
+# MySQL connection variables. Update this along with vars.php with your own settings..
+RECORDINGS_HOST        =    ""
+RECORDINGS_USER        =    ""
+RECORDINGS_PASSWORD    =    ""
+RECORDINGS_DB          =    ""
